6e523e3039e1f8b8592830ac62dbbecfee53736c
CJ Gilroy <cjgilroy@MacBook-Pro.local>
2019-03-09 13:53:00 -0700

    Change global body style

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,18 @@
 import App from '../components/App.js'
+import Head from 'next/head'
+import _JSXStyle from 'styled-jsx/style'
 export default () => (
+  <div>
+  <Head>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta charSet="utf-8" />
+</Head>
+<style jsx global>{`
+  body {
+    margin: 0;
+    font-family: sans-serif;
+  }
+`}</style>
   <App />
+  </div>
 )
