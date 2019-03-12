dd6cbddc1e80862d2f9eae14e03c0af74624faa7
CJ Gilroy <c.gilroy9@gmail.com>
2019-03-12 15:29:55 -0600

    update template

--- components/CardTemplate.js
+++ components/CardTemplate.js
@@ -3,20 +3,22 @@ import resources from './TeamResources.js'
 
 const outputCard = (props) => {
   return(
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 250 350">
+    <svg xmlns="http://www.w3.org/2000/svg" filter="url(#filter)" viewBox="0 0 250 350">
     <filter id="filter" x="-20%" y="-20%" width="140%" height="140%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" color-interpolation-filters="linearRGB">
-      <feTurbulence type="fractalNoise" baseFrequency="0.1 0.1" numOctaves="10" seed="4" stitchTiles="stitch" x="0%" y="0%" width="100%" height="100%" result="turbulence2"/>
-      <feColorMatrix type="matrix" values="0 1 0 0 0
-      0 1 0 0 0
-      0 1 0 0 0
-      0 1 0 0 0.6" x="0%" y="0%" width="100%" height="100%" in="turbulence2" result="colormatrix"/>
-      <feComposite in="SourceGraphic" in2="colormatrix" operator="in" x="0%" y="0%" width="100%" height="100%" result="composite1"/>
-    </filter>
+	    <feTurbulence type="fractalNoise" baseFrequency="0.5 0.5" numOctaves="1" seed="1" stitchTiles="noStitch" x="0%" y="0%" width="200%" height="100%" result="turbulence"/>
+	    <feTurbulence type="turbulence" baseFrequency="0.01 0.01" numOctaves="6" seed="1" stitchTiles="stitch" x="0%" y="0%" width="100%" height="100%" result="turbulence1"/>
+	    <feComposite in="turbulence" in2="turbulence1" operator="over" x="0%" y="0%" width="100%" height="100%" result="composite2"/>
+	     <feColorMatrix type="matrix" values="1 0 0 0 0
+          1 0 0 0 0
+          1 0 0 0 0
+          1 0 0 1 0"
+          x="0%" y="0%" width="100%" height="100%" in="composite2" result="colormatrix"/>
+	     <feComposite in="SourceGraphic" in2="colormatrix" operator="in" x="0%" y="0%" width="100%" height="100%" result="composite1"/>
+      </filter>
       <defs>
         <clipPath id="clip-path"><path id="clipPath" d="M192.73,343.83a39.11,39.11,0,0,1,37.56-68.62V68a12,12,0,0,0-12-12H16.69a12,12,0,0,0-12,12V331.83a12,12,0,0,0,12,12Z" style={{fill:'none',stroke:'#fff',strokeMiterLimit:'10px'}}/></clipPath>
       </defs>
-      <title>card-template</title>
-      <rect filter="url(#filter)" height="350" width="250" fill={resources[props.playerData.teamID].primaryColor}/>
+      <rect height="350" width="250" fill={resources[props.playerData.teamID].primaryColor}/>
       <g id="playerInfo">
         <g style={{clipPath:'url(#clip-path)'}}>
           <image
@@ -26,10 +28,10 @@ const outputCard = (props) => {
           <g>
             <rect transform="translate(0 300)" width="200px" height="70px" style={{fill:'#f7f5e8'}}>
             </rect>
-            <text  transform="translate(15 317)" style={{fontSize:'13px',fontFamily:'Arial-BoldMT, Arial',fontWeight:'700'}}>
+            <text  transform="translate(15 317)" style={{fontSize:'13px',fontFamily:'Arial-BoldMT, Arial',fontWeight:'700',pointerEvents:'none'}}>
               <tspan>{props.moreData[0].firstName.toUpperCase()}</tspan>
             </text>
-            <text fill="#e54f4a" transform="translate(15 335)" style={{fontSize:'18px',fontFamily:'Arial-BoldMT, Arial',fontWeight:'700'}}>
+            <text fill="#e54f4a" transform="translate(15 335)" style={{fontSize:'18px',fontFamily:'Arial-BoldMT, Arial',fontWeight:'700',pointerEvents:'none'}}>
               <tspan>{props.moreData[0].lastName.toUpperCase()}</tspan>
             </text>
           </g>
@@ -42,11 +44,11 @@ const outputCard = (props) => {
               style={{fill:'#f7f5e8',stroke:'#000',strokeMiterLimit:'10px'}}
           />
         <g id="data">
-          <text transform="translate(25 39.74)" style={{fontSize:'15px',fontFamily:'Arial-BoldItalicMT, Arial',fontWeight:'700',fontStyle:'italic'}}>
+          <text transform="translate(25 39.74)" style={{fontSize:'15px',fontFamily:'Arial-BoldItalicMT, Arial',fontWeight:'700',fontStyle:'italic',pointerEvents:'none'}}>
             <tspan textLength="195" style={{fill:'#e54f4a'}}>{resources[props.playerData.teamID].teamName.toUpperCase()}</tspan>
           </text>
           <g id="logo" transform="translate(88 286)"><svg height="47px">{resources[props.playerData.teamID].logo}</svg></g>
-          <text transform="translate(20 15)" style={{fontSize:'13px',fontFamily:'Arial-BoldMT, Arial',fontWeight:'700'}}>
+          <text transform="translate(20 15)" style={{fontSize:'13px',fontFamily:'Arial-BoldMT, Arial',fontWeight:'700',pointerEvents:'none'}}>
             <tspan fill="#fff">
               <tspan>#{props.playerData.number}</tspan>
               &#183;
