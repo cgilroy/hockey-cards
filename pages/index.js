13a680bac3399b24a4842ec2ef6c4ed59d2a287c
CJ Gilroy <c.gilroy9@gmail.com>
2019-04-29 14:21:06 -0600

    Remove erroneous Google analytics

--- pages/index.js
+++ pages/index.js
@@ -7,15 +7,6 @@ export default () => (
     <title>Hockey Cards</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-139240683-1"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'UA-139240683-1');
-    </script>
   </Head>
   <style jsx global>{`
     body {
